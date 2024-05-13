f4fda1735e9b72268e0151839099f5fbc10779ce
Faldukkan <fatimahkd97@gmail.com>
2024-05-13 18:42:13 +0300

    First

---
+++ Assets/scripts/Movment.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class Movment : MonoBehaviour
+{
+    private Rigidbody rd;
+    private float movX;
+    private float movY;
+    void Start()
+    {
+        rd = GetComponent<Rigidbody>();
+    }
+
+    void OnMove(InputValue movmentValue)
+    {
+        Vector2 movmetVector = movmentValue.Get<Vector2>();
+
+        movX = movmetVector.x;
+        movY = movmetVector.y;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movment = new Vector3(movX, 0f, movY);
+        rd.AddForce(movment);   
+    }
+
+
+
+}
